07caf347313ea0849e4be2e880f6fd3a431f3536
RichSphinx <112731283+RichSphinx@users.noreply.github.com>
2022-09-09 12:18:48 -0500

    Update bot
    Updating bot, adding dictionaries and usage.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # #! /usr/bin/env python3
 
+from ast import parse
+from logging import shutdown
 import os
 import telebot
+import telegram
+from telebot import types
 from urllib.request import urlretrieve
 import subprocess
 
@@ -10,22 +14,34 @@ bot = telebot.TeleBot(API_KEY)
 path = os.path.dirname(os.path.abspath(__file__))+"/upload/"
 
 
+markup = types.InlineKeyboardMarkup()
+usrs = types.InlineKeyboardButton('Users', callback_data="Users")
+passwd = types.InlineKeyboardButton('Passwords', callback_data="Passwords")
+markup.add(usrs, passwd)
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, """Usage:
-	1. Send the hash as a file and add what type of hash is as caption
-	2. Wait for answer""")
+    bot.send_message(message.chat.id, """*Usage:*
+    1. *Dictionaries:*
+        1.1 To add a dictionary just send the
+        dictionary with caption "*dict*" or 
+        "*dictionary*"
+	2. *Hash:*
+        2.1 Send the hash as a file and add
+        what type of hash is as caption
+        (*WPA*, *WPA2*, *NTLM*)
+	3. Wait for answer""", parse_mode='Markdown')
 
 
 def cracking(hashFile, crackingOption, dictionary):
-    if os.listdir(path):
-        result = subprocess.Popen(f'hashcat -m {crackingOption} -a 3 {hashFile} {dictionary} --quiet',
-                                  shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        stdout, stderr = result.communicate()
-        if not stderr:
-            return stdout.decode('utf-8')
-        else:
-            return stderr.decode('utf-8')
+    result = subprocess.Popen(f'hashcat -m {crackingOption} -a 3 {hashFile} {dictionary} --quiet',
+                                shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout, stderr = result.communicate()
+
+    if not stderr:
+        return stdout.decode('utf-8')
+    else:
+        return stderr.decode('utf-8')
 
 
 @bot.message_handler(content_types=['document'])
@@ -33,16 +49,21 @@ def handle_docs(message):
     fileName = message.document.file_name
     fileID = message.document.file_id
     file_info = bot.get_file(fileID)
-    urlretrieve(
-        f'https://api.telegram.org/file/bot{API_KEY}/{file_info.file_path}', path+fileName)
-    bot.reply_to(message, "Cracking...")
-    message.caption.upper()
-    if "WPA" in message.caption or "WPA2" in message.caption:
-        bot.reply_to(message, cracking(path+fileName, "22000", "?d?d?d?d?d?d?d?d"))
-    elif "NTLM" in message.caption:
-        bot.reply_to(message, cracking(
-            path+fileName, "1000", "?u?l?l?l?l?l?l?l?d"))
-    os.remove(path+fileName)
+    if "DICT" in message.caption.upper() or "DICTIONARY" in message.caption.upper():
+        urlretrieve(
+            f'https://api.telegram.org/file/bot{API_KEY}/{file_info.file_path}', path+'/dicts/'+fileName)
+        bot.reply_to(message, "Dictionary Saved")
+    else:
+        urlretrieve(
+            f'https://api.telegram.org/file/bot{API_KEY}/{file_info.file_path}', path+'hashes/'+fileName)
+        hashType, dictName = message.caption.split(" ")
+        if "WPA" in hashType.upper().strip() or "WPA2" in hashType.upper().strip():
+            bot.reply_to(message, "Cracking...")
+            bot.reply_to(message, cracking(path+'hashes/'+fileName, "22000", path+'dicts/'+dictName))
+        elif "NTLM" in hashType.upper().strip():
+            bot.reply_to(message, "Cracking...")
+            bot.reply_to(message, cracking(
+                path+'hashes/'+fileName, "1000", path+'dicts/'+dictName))
 
 
 bot.infinity_polling()
